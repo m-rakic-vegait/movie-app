ff8804a9620c76a09a0a1a6632d5c60893089e66
m-rakic-vegait <milan.rakic@vegait.rs>
2022-08-31 15:57:42 +0200

    Add search debounce

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Movie } from "../../interfaces";
+import { searchMovies } from "../../services/MovieService";
 import { InputWrap } from "./SearchInput.style";
 
-const SearchInput = () => {
+interface SearchField {
+    onSearch: (movies: Movie[]) => void;
+}
+
+const SearchInput = (props: SearchField) => {
+    const [value, setValue] = useState<string>('');
+
+    useEffect(() => {
+        if (value !== '') {
+            const delayDebounceFn = setTimeout(() => {
+                searchMovies(value).then((response) => {
+                    props.onSearch(response);
+                });
+            }, 500);
+
+            return () => clearTimeout(delayDebounceFn);
+        }
+    }, [value]);
+
     return (
         <InputWrap>
-            <input placeholder="Search by movie title" />
+            <input placeholder="Search by movie title" onChange={(e) => { setValue(e.target.value) }} value={value} />
         </InputWrap>
     );
 }
